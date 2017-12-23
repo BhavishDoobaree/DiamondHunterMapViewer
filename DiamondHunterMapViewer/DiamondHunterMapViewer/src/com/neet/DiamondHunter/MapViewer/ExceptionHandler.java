6178653715f91a2f8219565bfce1b7eaa16938fa
BhavishDoobaree <bdoobaree@gmail.com>
2017-12-23 22:39:58 +0800

    exceptionhandler
    exception handling module added to handle all exceptions, try catch,
    checks, etc

---
+++ DiamondHunterMapViewer/DiamondHunterMapViewer/src/com/neet/DiamondHunter/MapViewer/ExceptionHandler.java
@@ -0,0 +1,16 @@
+package com.neet.DiamondHunter.MapViewer;
+
+@SuppressWarnings("serial")
+public class ExceptionHandler extends Exception {
+	
+	  String excptxt;
+
+	    ExceptionHandler(String message){
+	        this.excptxt = message;
+	    }
+
+	    @Override
+	    public String getMessage(){
+	        return excptxt;
+	    }
+}
